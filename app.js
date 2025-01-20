7b4f08d22223a0a657b60af17ac2f11546972744
Anuska <anuskasth2022@gmail.com>
2025-01-21 10:33:08 +1100

    login access and transaction token created'

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+
 import { connectMongoDB } from "./config/mongoconfig.js";
 import bcrypt from "bcrypt";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+import jwt from "jsonwebtoken";
 
 //connect to mongoosedb
 connectMongoDB();
@@ -47,6 +50,37 @@ const userSchema = mongoose.Schema(
 );
 
 const User = mongoose.model("user", userSchema);
+
+const trasactionSchema = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["Income", "Expense"],
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Tranaction = mongoose.model("Transaction", trasactionSchema);
 
 app.post("/api/v1/users/register", async (req, res) => {
   try {
@@ -96,10 +130,19 @@ app.post("/api/v1/users/login", async (req, res) => {
     if (userData) {
       const loginSuccess = await bcrypt.compare(password, userData.password);
 
+      const tokenData = {
+        email: userData.email,
+      };
+
+      const token = await jwt.sign(tokenData, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+      });
+
       if (loginSuccess) {
         res.status(200).json({
           status: "success",
           message: " Login succesfull",
+          accessToken: token,
         });
       } else {
         res.status(403).json({
@@ -110,15 +153,25 @@ app.post("/api/v1/users/login", async (req, res) => {
     } else {
       res.status(404).json({
         status: "error",
-        message: "USer not found",
+        message: "User not found",
       });
     }
   } catch (error) {
-    res.send.status(500).json({
+    console.log(error);
+    res.status(500).json({
       status: "error",
       message: "login error",
     });
   }
+});
+
+//create transaction
+
+app.post("/api/v1/transactions", async (req, res) => {
+  res.status(201).json({
+    status: "success",
+    message: "transaction created",
+  });
 });
 
 app.listen(PORT, (error) => {
